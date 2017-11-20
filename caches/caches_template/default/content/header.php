603d98eaa71550a4eb06c12d146c4ca2f18c2b51
songlen <363912809@qq.com>
2017-11-20 21:40:13 +0800

    html

--- caches/caches_template/default/content/header.php
+++ caches/caches_template/default/content/header.php
@@ -20,8 +20,9 @@
 		<div class="menu fr">
 			<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=b43f1459ac702900c8d44c91a5e796dd&action=category&catid=0&num=25&siteid=%24siteid&order=listorder+ASC\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'category')) {$data = $content_tag->category(array('catid'=>'0','siteid'=>$siteid,'order'=>'listorder ASC','limit'=>'25',));}?>
 			<ul>
+				<li class="first"><a href="<?php echo siteurl($siteid);?>">首页</a></li>
 				<?php $n=1; if(is_array($data)) foreach($data AS $n => $r) { ?>
-				<li <?php if(($n == '1')) { ?>class="first"<?php } ?>>
+				<li>
 					<a href="<?php echo $r['url'];?>"><?php echo $r['catname'];?></a>
 					<?php if(defined('IN_ADMIN')  && !defined('HTML')) {echo "<div class=\"admin_piao\" pc_action=\"content\" data=\"op=content&tag_md5=806778b4a7cc16bc82ff203a0c4e5c24&action=category&catid=%24r%5Bcatid%5D&return=subcats\"><a href=\"javascript:void(0)\" class=\"admin_piao_edit\">编辑</a>";}$content_tag = pc_base::load_app_class("content_tag", "content");if (method_exists($content_tag, 'category')) {$subcats = $content_tag->category(array('catid'=>$r[catid],'limit'=>'20',));}?>
 
